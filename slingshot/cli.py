8f72092242c66409a67e903ffe8f8526d999cb66
Mike Graves <mgraves@mit.edu>
2016-02-01 11:09:30 -0500

    Updates documentation

--- slingshot/cli.py
+++ slingshot/cli.py
@@ -21,8 +21,25 @@ def main():
 @click.argument('store', type=click.Path(exists=True, file_okay=False,
                                          resolve_path=True))
 @click.argument('url')
-@click.option('--namespace', default='arrowsmith.mit.edu')
+@click.option('--namespace', default='arrowsmith.mit.edu',
+              help="Namespace used for generating UUID 5.")
 def run(layers, store, url, namespace):
+    """Create and upload bags to the specified endpoint.
+
+    This script will create bags from all the layers in the LAYERS
+    directory, uploading them to URL and storing them in the STORE
+    directory. Each layer should be in its own subdirectory.
+
+    Layers that already in exist in the STORE directory will be
+    skipped. This match is based solely on the names of the
+    subdirectories.
+
+    If a layer is not successfully uploaded it will not be placed in
+    the STORE directory.
+
+    The namespace option is used in generating a UUID 5 identifier
+    for the layer. The default value is arrowsmith.mit.edu.
+    """
     data = set(sub_dirs(layers))
     uploaded = set(sub_dirs(store))
     for directory in data - uploaded:
